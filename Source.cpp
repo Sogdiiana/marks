7bc17286bbc90148a60b7a01b757b5c41c54c0b8
Sogdiana <119744104+Sogdiiana@users.noreply.github.com>
2023-01-03 16:29:48 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout << "enter your mark--";
+	cin >> i;
+	while ( i < 10) 
+	{
+		cout << "enter your mark--";
+		cin >> i;
+		if (i ==2)
+		{
+			break;
+			
+		}
+        
+	}cout << "BAADDDDD";
+	return 0;
+}
